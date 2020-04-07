485bf4d095854235afdb6f41a3b3d60aa0b00207
fabio <fabiovalho@gmail.com>
2020-04-07 23:54:22 +0200

    the last commit before closing

--- exo_20.rb
+++ exo_20.rb
@@ -4,8 +4,17 @@ num = gets.to_i
 if num < 1 || num > 25
     puts "non! J'ai dit un numéro entre 1 et 25!"
 else
+    puts "#"
     x = 0
+    if num > 1
+        while x <= num
+        x = 0
     while x < num do
+        if num == 1
+            puts "#"
+            x = x + 1
+        else
+
         while x < (num - 1) do
             print "#"
             x = x + 1
